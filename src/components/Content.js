8b45afc487ff8654c4aba5286390cd4bb0ec5a0f
manish <manishkaswan88@gmail.com>
2024-09-09 09:36:41 +0530

    New commit

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,24 +2,88 @@ import React, { useEffect, useState } from 'react';
 import Card from './Card'; // Ensure the path is correct
 
 function Content() {
-  const [items, setItems] = useState([]);
+  const items = [
+    { 
+      title: "Join a living room session with Doja Cat", 
+      host: "Doja Cat", 
+      status: "Coming October", 
+      image: "https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp" 
+    },
+    { 
+      title: "Stay in Prince’s Purple Rain house kajjdgnajgdfnjk kagndjfnkgjn klangkdfngn", 
+      host: "Wendy and Lisa", 
+      status: "Coming September", 
+      image: "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" 
+    },
+    { 
+      title: "Sleepover at Polly Pocket’s Compact", 
+      host: "Polly Pocket", 
+      status: "Sold out", 
+      image: "https://images.ctfassets.net/kftzwdyauwt9/6Hpmny9K2Z8Xxget5bmlWa/66634b4c69faef5600e5ea48f499ba5e/Anastronautridingahorseinaphotorealisticstyle6.jpg?w=3840&q=90&fm=webp" 
+    },
+    { 
+      title: "Stay in Prince’s Purple Rain house kajjdgnajgdfnjk kagndjfnkgjn klangkdfngn", 
+      host: "Wendy and Lisa", 
+      status: "Coming September", 
+      image: "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" 
+    },
+    { 
+      title: "Join a living room session with Doja Cat", 
+      host: "Doja Cat", 
+      status: "Coming October", 
+      image: "https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp" 
+    },
+    { 
+        title: "Join a living room session with Doja Cat", 
+        host: "Doja Cat", 
+        status: "Coming October", 
+        image: "https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp" 
+      },
+      { 
+        title: "Stay in Prince’s Purple Rain house kajjdgnajgdfnjk kagndjfnkgjn klangkdfngn", 
+        host: "Wendy and Lisa", 
+        status: "Coming September", 
+        image: "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" 
+      },
+      { 
+        title: "Sleepover at Polly Pocket’s Compact", 
+        host: "Polly Pocket", 
+        status: "Sold out", 
+        image: "https://images.ctfassets.net/kftzwdyauwt9/6Hpmny9K2Z8Xxget5bmlWa/66634b4c69faef5600e5ea48f499ba5e/Anastronautridingahorseinaphotorealisticstyle6.jpg?w=3840&q=90&fm=webp" 
+      },
+      { 
+        title: "Stay in Prince’s Purple Rain house kajjdgnajgdfnjk kagndjfnkgjn klangkdfngn", 
+        host: "Wendy and Lisa", 
+        status: "Coming September", 
+        image: "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" 
+      },
+      { 
+        title: "Join a living room session with Doja Cat", 
+        host: "Doja Cat", 
+        status: "Coming October", 
+        image: "https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp" 
+      },
+      { 
+        title: "Stay in Prince’s Purple Rain house kajjdgnajgdfnjk kagndjfnkgjn klangkdfngn", 
+        host: "Wendy and Lisa", 
+        status: "Coming September", 
+        image: "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" 
+      },
+      { 
+        title: "Sleepover at Polly Pocket’s Compact", 
+        host: "Polly Pocket", 
+        status: "Sold out", 
+        image: "https://images.ctfassets.net/kftzwdyauwt9/6Hpmny9K2Z8Xxget5bmlWa/66634b4c69faef5600e5ea48f499ba5e/Anastronautridingahorseinaphotorealisticstyle6.jpg?w=3840&q=90&fm=webp" 
+      },
+      { 
+        title: "Stay in Prince’s Purple Rain house kajjdgnajgdfnjk kagndjfnkgjn klangkdfngn", 
+        host: "Wendy and Lisa", 
+        status: "Coming September", 
+        image: "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" 
+      },
+    // Add more items as needed
+  ];
   const [isMobileView, setIsMobileView] = useState(false);
-
-  // Fetch items from the API
-  useEffect(() => {
-    const fetchItems = async () => {
-      try {
-        const response = await fetch('https://airbnb-backend-1-iqk6.onrender.com/api/items'); // Adjust the URL to your API endpoint
-        const data = await response.json();
-        setItems(data);
-      } catch (error) {
-        console.error('Error fetching items:', error);
-      }
-    };
-
-    fetchItems();
-  }, []); // Empty dependency array ensures this runs once when the component mounts
-
   const handleResize = () => {
     setIsMobileView(window.innerWidth < 850);
   };
@@ -32,7 +96,7 @@ function Content() {
   }, []);
 
   return (
-    <div className={`grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-7 px-[5%] ${isMobileView ? 'mt-[180px]' : 'mt-[300px]'}`}>
+    <div className={`grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-7  px-[5%] ${isMobileView ? 'mt-[180px]' : 'mt-[300px]'}`}>
       {items.map((item, index) => (
         <Card 
           key={index} 
